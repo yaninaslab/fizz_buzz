2b2c6502428a39432caf99a830d63dd6a50074e5
yaninaslab <yana.belash@gmail.com>
2022-01-22 17:02:45 -0500

    Added elifs

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -1,14 +1,16 @@
 def fizz_buzz(num):
     if num % 3 == 0 and num % 5 == 0:
         print("FizzBuzz")
-    if num % 3 == 0:
+    elif num % 3 == 0:
         print("Fizz")
-    if num % 5 == 0:
+    elif num % 5 == 0:
         print("Buzz")
     else:
         print(num)
 
 
+num = 15
+fizz_buzz(num)
 numbers = [3, 5, 300, 15, 50, 60, 65, 33, 150, 120, 100, 160, 75, 155]
 
 for num in numbers:
